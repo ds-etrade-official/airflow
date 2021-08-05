5700c02bdd04f4b6b0929bfccc0575b70661aa67
Julie Rutherford <julie.a.rutherford@gmail.com>
2021-08-05 14:46:14 -0700

    Refine param documentation in the S3ListOperator

--- airflow/providers/amazon/aws/operators/s3_list.py
+++ airflow/providers/amazon/aws/operators/s3_list.py
@@ -36,9 +36,9 @@ class S3ListOperator(BaseOperator):
     :type prefix: str
     :param delimiter: The delimiter marks key hierarchy. (templated)
     :type delimiter: str
-    :param recursive: Recursively lists objects in a bucket, if set to True this
-    will include subfolders in addition to files. If set to False it will only
-    return files. Defaults to False.
+    :param recursive: If set to True this operator will return subfolders in
+    addition to files. If set to False only filenames will be returned. Defaults
+    to False to preserve backwards compatibility.
     :param aws_conn_id: The connection ID to use when connecting to S3 storage.
     :type aws_conn_id: str
     :param verify: Whether or not to verify SSL certificates for S3 connection.
@@ -102,14 +102,6 @@ class S3ListOperator(BaseOperator):
             self.delimiter,
             self.recursive,
         )
-
-        # keys = hook.list_keys(bucket_name=self.bucket, prefix=self.prefix, delimiter=self.delimiter)
-        # if self.recursive:
-        #     prefixes = hook.list_prefixes(
-        #         bucket_name=self.bucket, prefix=self.prefix, delimiter=self.delimiter
-        #     )
-        #     for prefix in prefixes:
-        #         keys.append(prefix)
 
         keys, prefixes = hook.list_keys(bucket_name=self.bucket, prefix=self.prefix, delimiter=self.delimiter)
         if self.recursive:
