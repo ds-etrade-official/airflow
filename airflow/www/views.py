9e7a95f9ffaa42d3ee0c6ccb1c4bfd49c1e2a2b1
Ash Berlin-Taylor <ash_github@firemirror.com>
2021-08-06 16:22:50 +0100

    Add missing permissions to varimport (#17468)
    (cherry picked from commit eb6af07f5bc8958efd06818e84a5273a079304e1)
    (cherry picked from commit 2bbaec8b76194ab95d569b066ae855167a4f18db)

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -3102,7 +3102,6 @@ class VariableModelView(AirflowModelView):
         'delete': 'delete',
         'action_muldelete': 'delete',
         'action_varexport': 'read',
-        'varimport': 'create',
     }
     base_permissions = [
         permissions.ACTION_CAN_CREATE,
@@ -3165,6 +3164,7 @@ class VariableModelView(AirflowModelView):
         return response
 
     @expose('/varimport', methods=["POST"])
+    @auth.has_access([(permissions.ACTION_CAN_CREATE, permissions.RESOURCE_VARIABLE)])
     @action_logging
     def varimport(self):
         """Import variables"""
